c598c2f4f8151065155658a310e76a19f514ef08
Ronald Camargo <ronaldcamargodev@gmail.com>
2024-04-26 08:55:24 -0400

    algumas modificações da modal

--- src/pages/dashboard/body/boxBest/modal/index.tsx
+++ src/pages/dashboard/body/boxBest/modal/index.tsx
@@ -1,15 +1,25 @@
+import "./style.css";
+
 type ModalBoxBestProps = {
     anime: any;
 }
 
 export default function ModalBoxBest({ anime }: ModalBoxBestProps) {
     return (
-        <div className="modal-content">
-            <h2>{anime.title}</h2>
-            <img src={anime.image} alt={anime.title} />
+        <div className="best-modal">
+            <div className="best-modal-title">
+                <h2>{anime.title}</h2>
+            </div>
+            <div className="best-modal-header">
+                <img src={anime.image} />
+                <div className="">
+
+                </div>
+                <p>{anime.description}</p>
+            </div>
             <p>Rating: {anime.rating}</p>
             <p>Public Rating: {anime.publicRating}</p>
         </div>
-    );
+    )
 }
 
