d7bbe6487da906a59e09f9d86f10478af6c40c47
Ronald Camargo <ronaldcamargodev@gmail.com>
2024-05-01 00:00:44 -0400

    nova att modal

--- src/pages/dashboard/body/boxBest/modal/index.tsx
+++ src/pages/dashboard/body/boxBest/modal/index.tsx
@@ -10,6 +10,7 @@ export default function ModalBoxBest({ anime }: ModalBoxBestProps) {
             <div className="best-modal-title">
                 <h2>{anime.title}</h2>
             </div>
+            <hr />
             <div className="best-modal-header">
                 <img src={anime.image} />
                 <div className="best-details">
@@ -22,8 +23,27 @@ export default function ModalBoxBest({ anime }: ModalBoxBestProps) {
                         <p>Rating: {anime.rating}</p>
                         <p>Public Rating: {anime.publicRating}</p>
                     </div>
-
-                    <p>{anime.description}</p>
+                    <div className="best-details-title-two">
+                        <p>Description</p>
+                    </div>
+                    <div className="area-text">
+                        <p>{anime.description}</p>
+                    </div>
+                    <div className="best-details-title-two">
+                        <p>Opinion</p>
+                    </div>
+                    <div className="area-text" style={{ borderRadius: '0 0 5px 5px' }}>
+                        <p>{anime.opinion}</p>
+                    </div>
+                </div>
+            </div>
+            <hr />
+            <div className="seasons">
+                <h2>Seasons</h2>
+                <div className="seasons-list">
+                    <div className="season-item">
+                        <h3>Title</h3>
+                    </div>
                 </div>
             </div>
         </div>
